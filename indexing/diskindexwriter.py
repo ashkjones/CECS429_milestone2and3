76ccada41b9c90ff10f54d4a01a7ac7df8ac5541
ashkjones <ashkjones@gmail.com>
2022-11-07 12:35:42 -0800

    diskindexwriter does writes to database now

--- indexing/diskindexwriter.py
+++ indexing/diskindexwriter.py
@@ -1,9 +1,11 @@
 from io import FileIO
-import struct
 from pathlib import Path
 from typing import Iterable
 from .positionalinvertedindex import PosInvertedIndex
 from .postings import Posting
+import struct
+import sqlite3
+
 
 class DiskIndexWriter:
    """Static class that can write a Positional Inverted Index to a binary file
@@ -13,18 +15,43 @@ class DiskIndexWriter:
       vocab = index.vocabulary()
       path = Path(dir, "postings.bin")
       file = open(path, "wb")
-      for term in vocab:
-         postings : Iterable[Posting] = index.get_postings(term) # shallow copy
-         df = len(postings)
-         file.write(struct.pack('>i', df))
 
-         doc_gap = 0
+      try: 
+         connection = sqlite3.connect("search_engine.db")
+         cursor = connection.cursor()
+         print("Connected to Database")
+         cursor.execute("DROP TABLE IF EXISTS TERM_POSITIONS")
 
-         # loop through the remaining postings
-         for post in postings:
-            file.write(struct.pack('>i', post.doc_id - doc_gap))
-            DiskIndexWriter.__pos_write(post, file)
-            doc_gap = post.doc_id
+         table = """ CREATE TABLE TERM_POSITIONS (
+                  TERM VARCHAR(255) NOT NULL,
+                  POSITION INT NOT NULL
+                  ); """
+
+         add_term = f"""INSERT into TERM_POSITIONS ('TERM', 'POSITION')
+                VALUES
+                ('{term}', {byte_pos})"""
+
+         cursor.execute(table)
+         for term in vocab:
+
+            # Let's record the term byte position to the database
+            byte_pos = file.tell()
+            cursor.execute(add_term)
+
+            postings : Iterable[Posting] = index.get_postings(term) # shallow copy
+            df = len(postings)
+            file.write(struct.pack('>i', df))
+
+            doc_gap = 0
+
+            # loop through the remaining postings
+            for post in postings:
+               file.write(struct.pack('>i', post.doc_id - doc_gap))
+               DiskIndexWriter.__pos_write(post, file)
+               doc_gap = post.doc_id
+
+      except:
+         print("Writing Index to file failed.")
 
 
    def __pos_write(post : Posting, file : FileIO):
